cd82612745d237712bd6bf486cc3b9aa81c365a0
zerofruit <14wnrkim@gmail.com>
2022-02-24 15:47:00 +0900

    impl DefaultChannelPromise

---
+++ transport/src/main/java/io/el/channel/DefaultChannelPromise.java
@@ -0,0 +1,39 @@
+package io.el.channel;
+
+import io.el.concurrent.DefaultPromise;
+import io.el.concurrent.EventLoop;
+import io.el.concurrent.Promise;
+import io.el.concurrent.PromiseListener;
+
+public class DefaultChannelPromise extends DefaultPromise<Void>
+    implements ChannelPromise {
+
+  private final Channel channel;
+
+  public DefaultChannelPromise(Channel channel, EventLoop eventLoop) {
+    super(eventLoop, () -> {});
+    this.channel = channel;
+  }
+
+  @Override
+  public Promise<Void> setSuccess(Void result) {
+    super.setSuccess(result);
+    return this;
+  }
+
+  @Override
+  public Channel channel() {
+    return channel;
+  }
+
+  @Override
+  public ChannelPromise addListener(PromiseListener<? extends Promise<? super Void>> listener) {
+    super.addListener(listener);
+    return this;
+  }
+
+  @Override
+  public ChannelPromise await() throws InterruptedException {
+
+  }
+}

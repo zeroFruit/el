3f52a7def7fcbdafdbfdbad0d6746cfb18ea6416
Park Juhyung <majecty@gmail.com>
2022-02-24 16:44:35 +0900

    remove unnecessary comment

--- transport/src/main/java/io/el/channel/ChannelHandlerContext.java
+++ transport/src/main/java/io/el/channel/ChannelHandlerContext.java
@@ -2,7 +2,6 @@ package io.el.channel;
 
 import io.el.concurrent.EventLoop;
 
-// event loop를 관리
 public interface ChannelHandlerContext extends ChannelInboundInvoker, ChannelOutboundInvoker {
   Channel channel();
   EventLoop eventLoop();

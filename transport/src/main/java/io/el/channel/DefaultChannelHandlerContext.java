064a32cef3dc32902645d9b5dfe7243247acccc9
zerofruit <14wnrkim@gmail.com>
2022-04-28 15:25:10 +0900

    feat: add DefaultChannelHandlerContext

---
+++ transport/src/main/java/io/el/channel/DefaultChannelHandlerContext.java
@@ -0,0 +1,19 @@
+package io.el.channel;
+
+import io.el.concurrent.EventLoop;
+
+public final class DefaultChannelHandlerContext extends AbstractChannelHandlerContext {
+
+  private final ChannelHandler handler;
+
+  DefaultChannelHandlerContext(String name, ChannelPipeline pipeline,
+      EventLoop eventLoop, ChannelHandler handler) {
+    super(name, pipeline, eventLoop, handler.getClass());
+    this.handler = handler;
+  }
+
+  @Override
+  public ChannelHandler handler() {
+    return handler;
+  }
+}

b3a1914f472a25a83ee63ef5d336731351367892
LarryKwon <32338616+raspLarry@users.noreply.github.com>
2022-05-12 16:19:42 +0900

    feat: implement ChannelSingleThreadEventLoop on DefaultChannelEventLoop (#61)

---
+++ transport/src/main/java/io/el/channel/DefaultChannelEventLoop.java
@@ -0,0 +1,24 @@
+package io.el.channel;
+
+import java.util.concurrent.Executor;
+
+public class DefaultChannelEventLoop extends ChannelSingleThreadEventLoop {
+
+  public DefaultChannelEventLoop(
+      Executor executor,
+      ChannelEventLoopGroup parent
+  ) {
+    super(executor, parent);
+  }
+
+  @Override
+  public void run() {
+    do {
+      Runnable task = takeTask();
+      if (task != null) {
+        task.run();
+        updateLastExecutionTime();
+      }
+    } while (!confirmShutdown());
+  }
+}
